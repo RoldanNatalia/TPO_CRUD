0cfeb848dc4e6ab5bbb23c7bd8e25f0c0395d84c
Natalia Roldan <111518783+RoldanNatalia@users.noreply.github.com>
2022-12-15 12:01:09 -0300

    se agregó el backend

---
+++ Backend/js/update.js
@@ -0,0 +1,53 @@
+var args = location.search.substr(1).split('&');
+console.log(location.search)
+// lee los argumentos pasados a este formulario
+var parts = []
+for (let i = 0; i < args.length; ++i) {
+    parts[i] = args[i].split('=');
+}
+console.log(args)
+document.getElementById("txtId").value = decodeURIComponent(parts[0][1])
+document.getElementById("txtTitulo").value = decodeURIComponent(parts[1][1])
+document.getElementById("txtImagen").value = decodeURIComponent(parts[2][1])
+document.getElementById("txtDescription").value =decodeURIComponent(parts[3][1])
+document.getElementById("txtSpotify").value = decodeURIComponent(parts[4][1])
+document.getElementById("txtYoutube").value = decodeURIComponent(parts[5][1])
+ 
+function modificar() {
+    let id = document.getElementById("txtId").value
+    let t = document.getElementById("txtTitulo").value
+    let i = document.getElementById("txtImagen").value
+    let d = document.getElementById("txtDescription").value
+    let s = document.getElementById("txtSpotify").value
+    let y = document.getElementById("txtYoutube").value
+    
+
+    let producto = {
+        name: t,
+        image: i,
+        preview: d,
+        spotify: s,
+        youtube:y
+        
+    }
+    let url = "http://localhost:5000/productos/"+id
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("modificado")
+            alert("Registro modificado")
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            console.error(err);
+            alert("Error al Modificar")
+        })      
+   
+ 
+}
